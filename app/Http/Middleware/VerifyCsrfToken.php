3236dafba60ad616d085b5f9996b3d154433f43d
agilbillah <agilbillah2@gmail.com>
2025-07-28 07:20:14 -0700

    Exclude .env from Git

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        '*', // Bebaskan semua endpoint dari verifikasi CSRF (AMAN untuk DEMO SAJA)
     ];
 }
